f34a3da8c84280dddeeeaffbab54c9ccd57b92f6
Stephen Crane <scrane88@gmail.com>
2021-10-04 14:38:30 -0400

    done

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
-
+  def artist_select(song)
+    binding.pry
+    label :song, :artist_name, "Artist Name"
+    if !params[:artist_id] && params[:action] == "edit"
+      collection_select :song, :artist_id, Artist.all, :id, :name
+    elsif params[:artist_id] && params[:action] == "new"
+      hidden_field_tag "song[artist_id]", song.artist_id
+    else
+      text_field :song, :artist_name
+    end
+  end
 end
